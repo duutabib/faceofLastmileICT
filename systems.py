4732064b1b8c57c59d2479223164993c287f90e7
garethinclusive <gareth@inclusive.energy>
2025-01-17 12:32:10 +0000

    Add .ipynb, .gitignore,

--- systems.py
+++ systems.py
@@ -11,14 +11,15 @@ data = pd.read_csv(dataPath)
 print(data.columns)
 print(data.dtypes)
 
-# labFit consansts
-A=0.12611,
-B=1.0134,
-C=0.0000051371,
+# labFit constants
+A=0.12611
+B=1.0134
+C=0.0000051371
 D=0.009961
 
-# Y=A/X2+B*EXP(C*X1)+D/X2**2
+#Y=A/X2+B*EXP(C*X1)+D/X2**2
 #Y=A*X1**(B*X2**C)+D/X2
+
 def labfit_function(row):
     return  -1 * A * row['Static_Pa'] ** (B * row['Differential_Pa']**C) + D / row['Differential_Pa']
 
@@ -26,7 +27,6 @@ def labfit_function(row):
 labfit_model = lambda row: -1*A*row.Static_Pa**(B*row.Differential_Pa**C) + D/row.Differential_Pa
 labfit_model0 = lambda row: -1*A*row.Differential_Pa**(B*row.Static_Pa**C) + D/row.Static_Pa
 
-data['Differential']
 data['labfit'] = data.apply(labfit_function, axis=1)
 
 print(data.columns)
