a53ec6e5a178071ba60bdfa59d52c2fc408a1135
duutabib <k.aduut@gmail.com>
2025-03-16 20:57:18 +0000

    Rename resource dir...

---
+++ inclusivePackage/src/data_sources.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Datasource(Enum):
+    def __init__(self):
+        raspberry_source = 1
+        other_soure = 2
+"""
+    need to figure out how exactly the data is coming in...
+"""
+
+if __name__ == "__main__":
