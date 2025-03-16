ec21bbfc9aad006e3ab26f89f629aba826ee8046
duutabib <k.aduut@gmail.com>
2025-03-16 23:07:35 +0000

    Modify __init__.py; __all__ with function signatures and import statements

--- inclusivePackage/src/__init__.py
+++ inclusivePackage/src/__init__.py
@@ -1,1 +1,25 @@
 print(f"Invoking __init__.py for {__name__}")
+
+__all__ = [
+    "data_analyzer", 
+    "data_manager",
+    "data_retriever",
+    "data_sources",
+    "data_transformer",
+    "data_utils",
+    "data_visualizer",
+]
+
+
+# for publishing
+from data_analyzer import DataAnalyzer
+from data_manager import DataManager
+from data_retriever import DataRetriever
+from data_sources import DataSource 
+from data_transformer import DataTransformer
+from data_utils import compute_residuals
+from data_utils import make_set_of_metrics
+from data_utils import compute_data_stats
+from data_utils import check_data_quality
+from data_visualizer import  DataVisualizer 
+from data_visualizer import ImageHandler
