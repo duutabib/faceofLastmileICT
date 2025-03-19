3896623e49c599cd637869c241630939d1c34bab
duutabib <k.aduut@gmail.com>
2025-03-19 17:30:34 +0000

    Add comments to methods...

--- inclusivePackage/src/data_writer.py
+++ inclusivePackage/src/data_writer.py
@@ -14,16 +14,17 @@ class DataWriter:
         self._data  = pandas_obj
     
     def save_csv(self, filename: Union[str, os.PathLike]) -> None:
-        "save data in csv format..."
+        """save data in csv format..."""
         with  open(filename, "wb") as canvas:
             self._data.to_csv(canvas)
 
     def save_excel(self, filename: Union[str, os.PathLike]) -> None:
-        'save data in excel format'
+        """save data in excel format"""
         with open(filename, "wb") as canvas:
             self._data.to_excel(canvas)
 
     def write_curr_best_data_to_file(self, filename: Union[str, os.PathLike] = "curr_best.pickle") -> None:
+        """Write best version of data in monitoring utility"""
         with open(filename, "wb") as canvas:
             pickle.dump(self._data, canvas,  protocol=pickle.HIGHEST_PROTOCOL)
         
