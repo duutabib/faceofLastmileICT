1a6535e14c47f9e2ed55db59b0520a60c9c5efd3
duutabib <k.aduut@gmail.com>
2025-03-19 15:35:28 +0000

    Refactor for better design; improving modularity, dependence and simplifying interface

--- inclusivePackage/src/data_manager.py
+++ inclusivePackage/src/data_manager.py
@@ -4,65 +4,72 @@ import typing
 
 import pandas as pd
 
-from typing import path, Any, optional
+from typing import Union, Any, Optional, List
+
+# I have two design choices to make, either I remove the Pandas decorator...
+# which expects a pandas object 
+# or keep it and abstract the reading of the data from the manager, so that the manager
+# expects a pandas object and not a filename name
+# so I could have a separate class that depends on that inherits from the pandas read data class
+# since the ddataManager depends on pandas functionality it might make sense to do the former...
+# otherwise, I could create my own removing the dependence on pandas, but this might be too costly
+# remove read functionality from data manager class...
 
 
 @pd.api.extensions.register_dataframe_accessor("manager")
-class DataManager(self):
+class DataManager:
     # I'm thinking of implementing usecols differently, what do you think? for example defining as a constant,
     # embedded in an assert statement
     # add handle invoking with missing filenames...
     # update: might be better to do the handling in pandas now...
-    def __init__(self, filename: typing.,  usecols=['Static Pressure', 'LPM Rota', 'temp_mcu', 'Static_Pa', 'SP_mV',
-       'DP_mV', 'Differential_Pa', 'Flow_lph', 'ideal flow ', 'cd',], **kwargs,):
-       if not filename:
-        raise ValueError("Cannot initialize Manager, filename not set or specified.")
-       self.filename = filename
-       self.usecols = usecols
-
-    def __call__(self) -> pd.DataFrame:
-        return self.data = pd.read_csv(self.filename, usecols=self.usecols, **kwargs)
-
-    def get_col(self, col: list ) -> pd.Series:
+    def __init__(self, pandas_obj):
+        """ Create a new Data Manager instance"""
+        self._data  = pandas_obj
+    
+    def get_col(self, col: Union[str List[str]] ) -> Union[pd.Series, pd.DataFrame]:
         'Implement one or many cols of data...'
-        if not all(item in self.data.columns.values for item in col):
-            raise KeyError(f"{col} not in data columns")
-        return self.data.loc[:, col] 
+        if isinstance(col, str):
+            col = [col]
+        if not all(item in self._data.columns.values for item in col):
+            raise KeyError(f"Columns {col} not in data columns.")
+        return self._data.loc[:, col] 
 
-    def get_row(self, row: typing.Union[str, int]) -> pd.Series:
+    def get_row(self, row: Union[str, int]) -> Union[pd.Series, pd.DataFrame]:
         'Implement for one or many row...'
-        if not all(item in self.data.columns.values for item in row):
-            raise KeyError(f"{row} not in data rows")
-        return self.data.loc[row,:]
+        if isinstance(row, [str, int]):
+            row = [row]
+        if not all(item in self.data.index for item in row):
+            raise KeyError(f"Rows {row} not in data rows")
+        return self._data.loc[row, :]
     
     def count_nrows(self) -> int:
         """
             returns the number of rows for data 
         """
-        return self.data.count()
+        return len(self._data.index)
 
     def count_ncols(self) -> int:
         "returns the number of cols for data"
-        return len(self.data.columns)       
+        return len(self._data.columns)       
 
     def deduplicate(self, inplace=False) -> pd.Series:
         "return data without duplicates..."
         if inplace:
-            self.data = self.data.drop_duplicates
-        return self.data.drop_duplicates
+            self._data.drop_duplicates(inplace=True)
+            return None
+        return self._data.drop_duplicates()
 
-
-    def save_csv(self, filename: typing.Union[str, os.PathLike]):
+    def save_csv(self, filename: Union[str, os.PathLike]) -> None:
         "save data in csv format..."
-        self.filename = filename
-        self.data.to_csv(self.filename)
+        with  open(filename, "wb") as canvas:
+            self._data.to_csv(canvas)
 
-    def save_excel(self, filename: typing.Union[str, os.PathLike]):
+    def save_excel(self, filename: Union[str, os.PathLike]) -> None:
         'save data in excel format'
-        self.filename = filename
-        self.data.to_excel(self.filename)
+        with open(filename, "wb") as canvas:
+            self._data.to_excel(canvas)
 
-
-    def write_curr_best_data_to_file(self) -> None:
-        pickle.dump(self.data, file="curr_best.pickle", pickle.HIGHEST_PROTOCOL)
+    def write_curr_best_data_to_file(self, filename: Union[str, os.PathLike] = "curr_best.pickle") -> None:
+        with open(filename, "wb") as canvas:
+            pickle.dump(self._data, canvas,  protocol=pickle.HIGHEST_PROTOCOL)
         
