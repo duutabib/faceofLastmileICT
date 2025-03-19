a321c2faecdd8d5740a0d64ae7f199eca2a9b037
duutabib <k.aduut@gmail.com>
2025-03-19 17:43:49 +0000

    Refactor; abstract data write features and to data_writer...

--- inclusivePackage/src/data_manager.py
+++ inclusivePackage/src/data_manager.py
@@ -59,17 +59,4 @@ class DataManager:
             return None
         return self._data.drop_duplicates()
 
-    def save_csv(self, filename: Union[str, os.PathLike]) -> None:
-        "save data in csv format..."
-        with  open(filename, "wb") as canvas:
-            self._data.to_csv(canvas)
-
-    def save_excel(self, filename: Union[str, os.PathLike]) -> None:
-        'save data in excel format'
-        with open(filename, "wb") as canvas:
-            self._data.to_excel(canvas)
-
-    def write_curr_best_data_to_file(self, filename: Union[str, os.PathLike] = "curr_best.pickle") -> None:
-        with open(filename, "wb") as canvas:
-            pickle.dump(self._data, canvas,  protocol=pickle.HIGHEST_PROTOCOL)
-        
+      
