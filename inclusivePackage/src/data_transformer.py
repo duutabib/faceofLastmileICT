87a77e2e69db9acc8694fb2122f95e88cf185669
duutabib <k.aduut@gmail.com>
2025-03-25 23:49:35 +0000

    Small changes...

--- inclusivePackage/src/data_transformer.py
+++ inclusivePackage/src/data_transformer.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-from typing import Optional, Any, List, 
+from typing import Optional, Any, List
 
 # consider making DataTransformer more general, so I can inherit Flow transformer.
 #
@@ -10,7 +10,7 @@ class DataTransformer:
     def __init__(self, pandas_obj: pd.DataFrame,  **kwargs):
         self._data= pandas_obj
 
-    def transform_variable(self, scaling_type: str = 'min-max', epsilon: float, axis: int = 1, **kwargs) -> pd.DataFrame:
+    def transform_variable(self, scaling_type: str = 'min-max', epsilon: float=10E-4, axis: int = 1, **kwargs) -> pd.DataFrame:
         """
         return scaled versions of desired variables [typically scales to (0, 1)]
         """
@@ -28,7 +28,7 @@ class DataTransformer:
            mean, std = col_data.mean(), col_data.std() + epsilon 
            return  (col_data - mean)/std
         
-       raise ValueError(f"Unknown scaling_type:{scaling_type}") 
+        raise ValueError(f"Unknown scaling_type:{scaling_type}") 
     
     def is_data_transformed(self, scaling_type: str, tolerance: float) -> bool:
         """
@@ -48,13 +48,13 @@ class DataTransformer:
             
             if scaling_type == 'standardized':
                 # Check mean and std for standardized data (mean ≈ 0, std ≈ 1)
-                mean, std = col_data.mean(), col_data.std()
+                mean, std_dev = col_data.mean(), col_data.std()
                 if not (abs(mean) < tolerance) and (abs(std_dev - 1) < tolerance):
                     return False
 
             elif scaling_type in ('normalized', 'min-max'):
                 # Check min and max for normalized data (min ≈ 0, max ≈ 1)
-                min_val, max_val = column_data.min(), column_data.max()
+                min_val, max_val = col_data.min(), col_data.max()
                 if not (abs(min_val) < tolerance) and (abs(max_val - 1) < tolerance):
                     return False
             else: 
@@ -67,15 +67,14 @@ class FlowTransformer:
         self._df = df
 
     def convert_to_flow_lph(self, col: str = 'Flow_lph') -> pd.Series:
+        """Convert flow from lph to lpm"""
         if col not in self._df:
             raise KeyError(f"Column {col} not found.")
-        return self._df[col]/60 
+        return self._df[col]* 0.0166
 
     def apply_lpm_rota_normalization(self, output_lpm: float, Tn: float, KCorrection: float) -> pd.DataFrame:
+        """Normalize lpm rotation ..."""
         required_cols = ["LPM_Rota", 'Static_Pa', 'temp_mcu']
         if not all(col in self._df for col in required_cols):
-            raise KeyError(f"Missing required columns {requied_cols}")
+            raise KeyError(f"Missing required columns {required_cols}")
         return self._df.LPM_Rota *(self._df.Static_Pa + output_lpm)/ output_lpm*(Tn/(self._df.temp_mcu + KCorrection))
-
-
-    
