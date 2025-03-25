efa48e5936b3c5466eec117eba8f9e3dd8f840c8
duutabib <k.aduut@gmail.com>
2025-03-25 12:54:00 +0000

    Small changes...

--- inclusivePackage/src/data_visualizer.py
+++ inclusivePackage/src/data_visualizer.py
@@ -1,6 +1,7 @@
-from typing import Optional, Union
+import pandas as pd
 
 import matplotlib.pyplot as plt
+from typing import Optional, Union
 import matplotlib.gridspec as gridspec
 
 
@@ -16,21 +17,32 @@ class DataVisualizer:
             raise ValueError(f"Invalid fit_dict format for {model_key}")
             
 
-    def _plot_model(self,ax, y_actual, y_pred, score, title: str, color:str):
+    def _plot_model(self,ax, y_actual, y_pred, score, mse, title: str, color:str):
         ax.scatter(y_actual, y_pred, color=color)
         ax.axline((0, 0), slope=1, linestyle'--', color='red', linewidth=2)
-        ax.set_title(f"{title} (Score ={score:.3f})", fontsize)
+        ax.set_xlabel("X instances in time", fontsize=20)
+        ax.set_title(f"Discharge Coefficient", fontsize=20)
+        ax.set_title(f"{title} (Score ={score:.3f}) (mse ={mse:.3f})", fontsize=20)
 
 
     # maybe implement helper for plot asthetics including (axis, labels)
+    # not sure able about this in this alpha version
     def _plot_decorator(labels, ax, bounds, ):
         ...
+
+    # need to figure out where I set this...
+    TITLE = [
+            "$f(x, y) = a x^2 + b xy + cx+ dy+ ey^2 + f$: y-actual vs y_pred",
+            "DecisionTreeRegressor: y-actual vs y_pred", 
+            "actual model: y-actual vs y_pred" 
+    ]
 
     def apply_model_visualizer(self, models, color: str ='blue', figsize: tuple = (20, 20), all: bool, **kwargs) -> Optional[list]:
             # Implementation: how to handle compare for specific type of models different 
             # need to think a bit about the design... 
             # create helper functions for each visualization of a model compose for the compare
             # that might work...
+            """Visual model predictions against actual data."""
 
             """
                 returns a plot of all model fits for data, with the default all flag. Working on
@@ -45,46 +57,19 @@ class DataVisualizer:
             models = ['lm_model', 'poly_model', 'dTree_model'] if all else []
             models  = models or self._fit_dict.keys() - {y_actual}
                         
-            # define data (fix outputs for model_mse)
-            _, _, y_actual = _get_model_data(model='y_actual')
-            lm_score, lm_mse, ypred_lm = _get_model_data(model='lm_model')
-            _, poly_mse, poly_ypred = _get_model_data(model='poly_model')
-            dTree_score, dTree_mse,  ypred_dTree = _get_model_data(model='dTree_model')
-            
-
             fig0 = plt.figure(figsize= figsize, constrained_layout=True)
-            ncols= 1, len(models)
+            ncols=len(models)
             w1, w2 = 20, 1
             gspec=gridspec.GridSpec(ncols=ncols, nrows=1, figure=fig0, width_ratios=[w1, w2]))
+            y_actual = self.fit_dict.get('y_actual', pd.Series())
 
-            # 
-            ax = plt.subplot(gspec[0, 0], aspect=1)
-            ax.set_xlim(0, 1)
-            ax.set_xticks(np.linspace(0, 1, 4 + 1))
-            ax.set_xlabel("X instances in time")
-            ax.set_ylim(0, 1)
-            ax.set_yticks(np.linspace(0, 1, 4 + 1))
-            ax.set_ylabel("Discharge Coefficient", fontsize=20)
-            ax.set_title("Title", family="Roboto", weight=500)
-            ax.axline((0.00, 0.00), slope=1.0, linestyle='--', color='red', linewidth=2.0)
-            ax.set_title(f"actual model: y-actual vs y_pred ({ lm_score=:.3f}) {lm_mse=:.3f}", fontsize=20)
-            I = ax.scatter(y_actual, y_lm, color=color)
-
-            # Decision tree compare
-            ax = plt.subplot(gspec[0, 1], aspect=1)
-            ax.set_xlabel("X instances in time", fontsize=20)
-            ax.set_title(f"DecisionTreeRegressor: y-actual vs y_pred ({dTree_score=:.3f} {dTree_mse=:.3f})", fontsize=20)
-            ax.axline((0.0, 0.0), slope=1, linestyle='--', color='red', linewidth=2.0)
-            ax.scatter(y_actual, y_dTree, color=color)
-
-            # poly plot 
-            ax = plt.subplot(gspec[0, 2], aspect=1)
-            ax.set_xlabel("X instances in time", fontsize=20)
-            ax.axline((0.0, 0.0), slope=1, linestyle='--', color='red', linewidth=2.0)
-            ax.set_title(f"$f(x, y) = a x^2 + b xy + cx+ dy+ ey^2 + f$: y-actual vs y_pred (R_sq={None} {poly_mse=:.3f})", fontsize=20)
-            I = ax.scatter(y_actual, y_poly, color=color)
-        return I
-
+            for i, model_key in enumerate(models):
+                score, mse, y_pred = _get_model_data(model=model_key)
+                ax = fig.add_subplot(gspec[0, i])
+                self._plot_model(ax, y_actual, y_pred, score, mse, model_key.captialize(), color)
+            plt.show()
+    
+            
 
 # takes objects from the  visualizer, resize and saves it one of two formats {png, pdf}.
 class ImageHandler:
@@ -95,7 +80,7 @@ class ImageHandler:
         self.name = name
 
     # resize image
-    def _resize_image(self, width: int, height:int) -> None:
+    def resize(self, width: int, height:int) -> None:
         self._fig.set_size_inches(width, height)
 
     # save image as pdf
