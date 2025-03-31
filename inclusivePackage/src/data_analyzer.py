8c52512f9f856f6c82af2f1fa0e7d00c325b7a15
duutabib <k.aduut@gmail.com>
2025-03-31 15:09:51 +0100

    Reformat data_analyzer.py...

--- inclusivePackage/src/data_analyzer.py
+++ inclusivePackage/src/data_analyzer.py
@@ -10,9 +10,9 @@ from sklearn.tree import DecisionTreeRegressor
 class Analyzer:
     def __init__(self, pandas_obj: pd.DataFrame):
         self._pandas_obj = pandas_obj
-  
+
     class FitResult:
-        def __init__(self, score:float, mse:float, predictions:NDArray):
+        def __init__(self, score: float, mse: float, predictions: NDArray):
             self.score = score
             self.mse = mse
             self.predictions = predictions
@@ -20,90 +20,95 @@ class Analyzer:
     def _make_fit_result(self, score: float, mse: float, predictions: NDArray) -> tuple:
         "returns an set of model metrics; including predictions"
         return self.FitResult(score, mse, predictions)
-    
 
-    def _fit_model(self, model, X: NDArray, y: NDArray) -> 'FitResult':
-        """ Compute model score, mean squure error and predictions
+    def _fit_model(self, model, X: NDArray, y: NDArray) -> "FitResult":
+        """Compute model score, mean squure error and predictions
 
-            Args:
-                model_obj: model object, initialized model object 
-                X (NDArray): (n, 2) n instances of 2 features from 
-                y (NDArray): (n,) instances of dependent variable 
-            
-            Returns:
-                an ordered tuple, score, mse, y_pred
-         """
+        Args:
+            model_obj: model object, initialized model object
+            X (NDArray): (n, 2) n instances of 2 features from
+            y (NDArray): (n,) instances of dependent variable
+
+        Returns:
+            an ordered tuple, score, mse, y_pred
+        """
         model.fit(X, y)
-        score = model.score(X, y) 
+        score = model.score(X, y)
         y_pred = model.predict(y)
-        mse = self.compute_mse(y, y_pred) 
+        mse = self.compute_mse(y, y_pred)
         return self.FitResult(score, mse, y_pred)
 
     @staticmethod
-    def compute_mse(y_actual: NDArray[np.float64], y_predicted: NDArray[np.float64]) -> float:
+    def compute_mse(
+        y_actual: NDArray[np.float64], y_predicted: NDArray[np.float64]
+    ) -> float:
         """Compute mean squared error between actual and predicted values.
-            Return mse, residuals  for model predictions.
+        Return mse, residuals  for model predictions.
         """
         residuals = y_actual - y_predicted
-        return np.mean(residuals ** 2)
+        return np.mean(residuals**2)
 
-
-    @staticmethod 
+    @staticmethod
     def x_design(X: NDArray, terms: list[str]) -> NDArray:
-        """ Creates design matrix X based on terms.
-            Args:
-                X (NDArray[n, 2]): shape (n, 2) or (number of instances, number of features) 
-                terms List[str] : form of the function. Default is f(x0, x1)=a* x0^2 + b* x0 + c* x0*x1 + d* x1 + e*x1^2 + f
-            Returns:
-                an array design X for which is of the form 
+        """Creates design matrix X based on terms.
+        Args:
+            X (NDArray[n, 2]): shape (n, 2) or (number of instances, number of features)
+            terms List[str] : form of the function. Default is f(x0, x1)=a* x0^2 + b* x0 + c* x0*x1 + d* x1 + e*x1^2 + f
+        Returns:
+            an array design X for which is of the form
         """
-        if X.shape[1] !=2:
+        if X.shape[1] != 2:
             raise ValueError("X must have exactly 2 features")
         x0 = X[:, 0]  # static pressure
-        x1 = X[:, 1]  # differential pressure 
-        default_terms= ['1', 'x0', 'x1', 'x0^2', 'x0*x1', 'x1^2']
+        x1 = X[:, 1]  # differential pressure
+        default_terms = ["1", "x0", "x1", "x0^2", "x0*x1", "x1^2"]
         terms = terms or default_terms
-        design = [np.ones_like(x0) if t == '1' else eval(t, {'x0': x0, 'x1': x1}) for t in terms]
-        return  np.vstack(design).T
+        design = [
+            np.ones_like(x0) if t == "1" else eval(t, {"x0": x0, "x1": x1})
+            for t in terms
+        ]
+        return np.vstack(design).T
 
+    def fit_data(
+        self,
+        x_cols: list[str],
+        y_col: str,
+        models: dict[str] = None,
+        random_state: int = 42,
+    ) -> dict:
+        """Fit multiple models to data.
+        Args:
+            x_cols (list[str]) : list of cols of x.
+            y_col: (n, ) instances of response variable
+            models: dict of models
+            random_state (int): integer to set the random state of model for reproducibility.d
 
+        Returns:
+            return a dictionary `fitDict` of multiple fits for data... including:
+            polynomial fit, decision trees, regresssion
 
-
-    def fit_data(self, x_cols: list[str], y_col: str, models: dict[str]= None, random_state: int = 42) -> dict:
-        """ Fit multiple models to data.
-            Args: 
-                x_cols (list[str]) : list of cols of x.
-                y_col: (n, ) instances of response variable
-                models: dict of models 
-                random_state (int): integer to set the random state of model for reproducibility.d
-
-            Returns:
-                return a dictionary `fitDict` of multiple fits for data... including:
-                polynomial fit, decision trees, regresssion 
-            
         """
         default_models = {
-            'lm_model': LinearRegression(random_state=random_state),
-            'dTree_model': DecisionTreeRegressor(random_state=random_state)
+            "lm_model": LinearRegression(random_state=random_state),
+            "dTree_model": DecisionTreeRegressor(random_state=random_state),
         }
 
         X = self._pandas_obj[x_cols].to_numpy()
         y = self._pandas_obj[y_col].to_numpy()
 
         # init, fit and predict linear regression  model
-        # define helper to compute model stats... 
+        # define helper to compute model stats...
         # call to and add metrics as
 
-        models = models or default_models 
-        fit_dict = {'y_actual': self._make_fit_result(None, None, y)}
+        models = models or default_models
+        fit_dict = {"y_actual": self._make_fit_result(None, None, y)}
         for name, model in models.items():
             fit_dict[name] = self._fit_model(model, X, y)
-        if 'poly_model' not in models:
+        if "poly_model" not in models:
             x_design = self.x_design(X, terms=None)
-            coef, *_= np.linalg.lstsq(x_design, y)
+            coef, *_ = np.linalg.lstsq(x_design, y)
             y_pred = x_design @ coef
             poly_mse = self.compute_mse(y, y_pred)
-            fit_dict['poly_model'] = self._make_fit_result(None, poly_mse, poly_y)
+            fit_dict["poly_model"] = self._make_fit_result(None, poly_mse, poly_y)
 
         return fit_dict
-
