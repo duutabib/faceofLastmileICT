b8e98458a49fc1c84e78be73accf535eee5faffa
duutabib <k.aduut@gmail.com>
2025-04-01 14:09:20 +0100

    Add comments...

--- inclusivePackage/src/data_sources.py
+++ inclusivePackage/src/data_sources.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
 class Source(Enum):
+    """Class representing the sources of data... 
+    """
     def __init__(self):
         raspberry_source = 1
         other_soure = 2
-"""
-    need to figure out how exactly the data is coming in...
-"""
+    # need to figure out how exactly the data is coming in...
 
 if __name__ == "__main__":
+    ...
