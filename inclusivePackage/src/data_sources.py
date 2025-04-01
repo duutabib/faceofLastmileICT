96174f3645d7a1e4a3c5112d7af9e6dd93c9083a
duutabib <k.aduut@gmail.com>
2025-04-01 14:09:38 +0100

    Fix typo...

--- inclusivePackage/src/data_sources.py
+++ inclusivePackage/src/data_sources.py
@@ -5,7 +5,7 @@ class Source(Enum):
     """
     def __init__(self):
         raspberry_source = 1
-        other_soure = 2
+        other_source = 2
     # need to figure out how exactly the data is coming in...
 
 if __name__ == "__main__":
