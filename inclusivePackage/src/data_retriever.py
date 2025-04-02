e524bbf38e1a930b3f3e0b0f00d46b505dd621c3
duutabib <k.aduut@gmail.com>
2025-04-02 11:32:17 +0100

    Add minimal docstring, type hints...

--- inclusivePackage/src/data_retriever.py
+++ inclusivePackage/src/data_retriever.py
@@ -1,17 +1,21 @@
+import Enum 
 
-class DataRetriever:
-    def __init__(self, source= [Enum], is_connected):
+class Retriever:
+    """Class representing DataRetriever"""
+    def __init__(self, source: [Enum] = None, is_connected: bool = False):
         self.source = source
         self.is_connected = is_connected
 
     def connector(self):
+        # establish connection
         if not self.is_connected:
             'establish connection'
         return self.is_connected
 
     def get_data(self):
+        # get data from source
         # data from source
-        if connector():
+        if self.connector():
             'source data and write to csv or EXcel'
             'More details to implement...'
         return 0 
