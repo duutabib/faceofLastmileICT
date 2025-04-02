9ace60969548c82d050e1322ac0ed86082bc20fb
duutabib <k.aduut@gmail.com>
2025-04-02 13:20:46 +0100

    Rename normalization to normalized in line 10, adjusted endpoints of range...

--- inclusivePackage/test/test_transformer.py
+++ inclusivePackage/test/test_transformer.py
@@ -1,1 +1,24 @@
+"""Module for testing transformer class"""
 import pytest
+from src.data_transformer import Transformer
+
+
+@pytest.mark.parametrize(
+    'scaling_type',
+    [
+        'min-max', 
+        'standardized',
+        'normalized',
+    ],
+)
+def test_transform_variable(pandas_obj, scaling_type):
+    """test for transformer class, ensures that the data we
+        getting the right out put...
+
+    """
+    data = Transformer(pandas_obj)
+    data = data.transform_variable(scaling_type)
+    assert data.iloc[:, 0].max() <= 2
+    assert data.iloc[:, 0].min() >= -2
+
+    
