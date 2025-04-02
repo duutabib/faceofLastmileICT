5ed967eed08302dbd6df954b5e0a17bab692e1ea
duutabib <k.aduut@gmail.com>
2025-04-02 11:16:00 +0100

    Fix errors in counts for test... all test passed...

--- inclusivePackage/test/test_data_manager.py
+++ inclusivePackage/test/test_data_manager.py
@@ -7,7 +7,7 @@ def test_manager_get_col(pandas_obj):
     getting the right out put...
     """
     assert Manager(pandas_obj).get_col(["Static Pressure", "ideal flow"]).shape == (
-        891,
+        246,
         2,
     )
 
@@ -15,7 +15,7 @@ def test_manager_get_row(pandas_obj):
     """test for get_row , ensures that the data we
     getting the right out put...
     """
-    assert Manager(pandas_obj).get_row(1).shape == (1, 8)
+    assert Manager(pandas_obj).get_row(1).shape == (1, 10)
 
 def test_manager_count_nrows(pandas_obj):
     """test for get_row , ensures that the data we
@@ -28,7 +28,7 @@ def test_manager_deduplicate(pandas_obj):
     """test manager deduplicate, ensures that the data we
     getting the right out put...
     """
-    assert Manager(pandas_obj).deduplicate().shape == (230, 10)
+    assert Manager(pandas_obj).deduplicate().shape == (210, 10)
 
 
 def test_manager_count_ncols(pandas_obj):
