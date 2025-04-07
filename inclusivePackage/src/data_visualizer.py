57765b3ee43bdfcaa4918d20d31860582c06a764
duutabib <k.aduut@gmail.com>
2025-04-07 12:27:21 +0100

    Fix typo in line 22, rename class line 8, small cleanup...

--- inclusivePackage/src/data_visualizer.py
+++ inclusivePackage/src/data_visualizer.py
@@ -5,7 +5,7 @@ from typing import Optional, Union
 import matplotlib.gridspec as gridspec
 
 
-class DataVisualizer:
+class Visualizer:
     def __init__(self,  fit_dict: dict = None, **kwargs,  ):
         self._fit_dict = fit_dict
 
@@ -19,7 +19,7 @@ class DataVisualizer:
 
     def _plot_model(self,ax, y_actual, y_pred, score, mse, title: str, color:str):
         ax.scatter(y_actual, y_pred, color=color)
-        ax.axline((0, 0), slope=1, linestyle'--', color='red', linewidth=2)
+        ax.axline((0, 0), slope=1, linestyle='--', color='red', linewidth=2)
         ax.set_xlabel("X instances in time", fontsize=20)
         ax.set_title(f"Discharge Coefficient", fontsize=20)
         ax.set_title(f"{title} (Score ={score:.3f}) (mse ={mse:.3f})", fontsize=20)
@@ -34,45 +34,45 @@ class DataVisualizer:
     TITLE = [
             "$f(x, y) = a x^2 + b xy + cx+ dy+ ey^2 + f$: y-actual vs y_pred",
             "DecisionTreeRegressor: y-actual vs y_pred", 
-            "actual model: y-actual vs y_pred" 
+            "actual model: y-actual vs y_pred",
     ]
 
     def apply_model_visualizer(self, models, color: str ='blue', figsize: tuple = (20, 20), all: bool, **kwargs) -> Optional[list]:
-            # Implementation: how to handle compare for specific type of models different 
-            # need to think a bit about the design... 
-            # create helper functions for each visualization of a model compose for the compare
-            # that might work...
-            """Visual model predictions against actual data."""
+        # all param (bool) = control number of fits compared...
 
-            """
-                returns a plot of all model fits for data, with the default all flag. Working on
-                additional flags to allow for flexibilty in comparison. 
-                Args:
-                    color: string; set color for labels
-                    figsize: tuple; set size for the fig
-                    all: bool; rendering comparison for all models 
-                    **kwargs:
-            """
+        """Visual model predictions against actual data."""
 
-            models = ['lm_model', 'poly_model', 'dTree_model'] if all else []
-            models  = models or self._fit_dict.keys() - {y_actual}
-                        
-            fig0 = plt.figure(figsize= figsize, constrained_layout=True)
-            ncols=len(models)
-            w1, w2 = 20, 1
-            gspec=gridspec.GridSpec(ncols=ncols, nrows=1, figure=fig0, width_ratios=[w1, w2]))
-            y_actual = self.fit_dict.get('y_actual', pd.Series())
+        """
+            returns a plot of all model fits for data, with the default all flag. Working on
+            additional flags to allow for flexibilty in comparison. 
+            Args:
+                color: string; set color for labels
+                figsize: tuple; set size for the fig
+                all: bool; rendering comparison for all models 
+                **kwargs:
+        """
 
-            for i, model_key in enumerate(models):
-                score, mse, y_pred = _get_model_data(model=model_key)
-                ax = fig.add_subplot(gspec[0, i])
-                self._plot_model(ax, y_actual, y_pred, score, mse, model_key.captialize(), color)
-            plt.show()
+        models = ['lm_model', 'poly_model', 'dTree_model'] if all else models
+        models  = models or self._fit_dict.keys() - {y_actual}
+                    
+        fig0 = plt.figure(figsize= figsize, constrained_layout=True)
+        ncols=len(models)
+        w1, w2 = 20, 1
+        gspec=gridspec.GridSpec(ncols=ncols, nrows=1, figure=fig0, width_ratios=[w1, w2]))
+        y_actual = self.fit_dict.get('y_actual', pd.Series())
+
+        for i, model_key in enumerate(models):
+            score, mse, y_pred = _get_model_data(model=model_key)
+            ax = fig.add_subplot(gspec[0, i])
+            self._plot_model(ax, y_actual, y_pred, score, mse, model_key.captialize(), color)
+        plt.show()
     
             
 
-# takes objects from the  visualizer, resize and saves it one of two formats {png, pdf}.
 class ImageHandler:
+    """ takes objects from the  visualizer, resize 
+    and saves it one of two formats {png, pdf}.
+    """
     def __init__(self, fig: plt.Figure, name: str) -> None:
         if not isinstance(figure, plt.Figure):
             raise TypeError("figure must be a matplotlib.pyplot.Figure")
