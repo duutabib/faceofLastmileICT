0cf1116f53f9fcd4d974b7e8686e7f63fccb67f5
duutabib <k.aduut@gmail.com>
2025-04-07 16:41:54 +0100

    Add test for analyzer... fixed issues...

--- inclusivePackage/test/test_analyzer.py
+++ inclusivePackage/test/test_analyzer.py
@@ -1,1 +1,7 @@
-import pytest
+"""Module for testing the analyzer..."""
+from src.data_analyzer import Analyzer
+
+
+def test_fit_data(pandas_obj, x_cols= ['Static_Pa', 'Differential_Pa'], y_col='cd', models=None):
+    """Test fit data, ensure the return type is right..."""
+    assert  'True'== isinstance(Analyzer(pandas_obj).fit_data(x_cols, y_col),  dict)
